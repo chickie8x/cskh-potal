96d7aefe03f77f879c5e0337e6148939b2064fb4
chickie8x <chickie8x.qn@gmail.com>
2025-12-20 13:37:47 +0700

    update

---
+++ dist/assets/index-DnF_xtfv.js
@@ -0,0 +1,1 @@
+import{au as o,av as a,aw as r}from"./index-BoRylNbp.js";function c(){r({variableName:a("scrollbar.width").name})}function s(){o({variableName:a("scrollbar.width").name})}export{c as b,s as u};
